1a4c0d28ca9ef6a2aa6b554f3cb24a8fd42ba11a
FilipeRop <filiperopecalima@gmail.com>
2023-02-01 11:05:54 -0300

    Exercício 2

---
+++ Ex-02.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].#
+
+num = int(input('Escreva um número inteiro: ')) #recebe, converte e armazena o valor na variável.
+
+print(f'O número informado foi {num}') #exibe o valor no terminal
