3b3ea8f972c5c897e2defd787637b3a9f59c4e24
FilipeRop <filiperopecalima@gmail.com>
2023-02-01 11:32:46 -0300

    Exercício 3

---
+++ Ex-03.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma.#
+
+num_1 = float(input('Escreva um número: '))
+num_2 = float(input('Escreva mais um número: '))
+
+soma = num_1 + num_2
+
+print(f'A soma de {num_1} + {num_2} é igual a {soma}.')
