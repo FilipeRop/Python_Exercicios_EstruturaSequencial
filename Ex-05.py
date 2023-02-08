9c4f74659a0226cf96eab32644c1013dfa140b80
FilipeRop <filiperopecalima@gmail.com>
2023-02-08 13:10:14 -0300

    Exercício 5

---
+++ Ex-05.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.#
+
+valor_Cm = float(input('Valor em centimetros => '))
+
+valor_Met = valor_Cm/100
+
+print(f'Valor em metros => {valor_Met}')
